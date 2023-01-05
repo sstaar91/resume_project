379c6e63d93f756f8bb0d8a2b68501f52527e106
sstaar91 <sstaar91@gmail.com>
2023-01-06 00:02:11 +0900

    ADD : Home 초기세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import Home from 'pages/Home';
 
 const Router = () => {
   return (
