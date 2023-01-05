bef28e3aa66c437ff78188e9d0cf3a81aab295e7
sstaar91 <sstaar91@gmail.com>
2023-01-05 17:26:54 +0900

    ADD : Router 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Router from './Router';
+import './styles/reset.scss';
+import './styles/common.scss';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+root.render(<Router />);
